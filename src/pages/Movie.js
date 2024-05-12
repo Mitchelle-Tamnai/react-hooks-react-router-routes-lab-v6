aed15762ea69d65231508b78500b9cb77ba4cd27
MitchelleTamnai <cheptoektamnai@gmail.com>
2024-05-12 23:35:35 +0300

    Commit Changes

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,33 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const params = useParams();
+  const movieId = params.id;
+  const [movies, setMovies] = useState({});
+  
+
+  useEffect(()=> {
+    fetch(`http://localhost:4000/movies/${movieId}`)
+    .then((res) => res.json())
+    .then((data) => setMovies(data))
+  },[movieId])
+
+  if(!movies.title){
+    return <p>Loading...</p>
+  }
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar/>
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movies.title}</h1>
+        <p>{movies.time} mins</p>
+        {movies.genres.map((genre, index) => (
+          <span key={index}>{genre}</span>
+        ))}
       </main>
     </>
   );
